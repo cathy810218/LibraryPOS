e57eadea0e373cd81270bd598bac6d92a7cb4c8a
cathy810218 <cathy810218@gmail.com>
2017-06-07 19:37:51 -0700

    Add customer

--- src/main/java/com/codefellows/Customer.java
+++ src/main/java/com/codefellows/Customer.java
@@ -3,5 +3,18 @@ package com.codefellows;
 /**
  * Created by cathyoun on 6/7/17.
  */
-public class Customer {
+public class Customer extends Person {
+    public int age;
+    public Customer(String firstName, String lastName, int age) {
+        super(firstName,lastName,age);
+    }
+
+    public void doWork() {
+        System.out.println("Shopping!");
+    }
+
+    @Override
+    public String fullName() {
+        return "Our valued customer: " + super.fullName();
+    }
 }
